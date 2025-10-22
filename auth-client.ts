115808d62b487125dfcfc38eeda51b84a01e82c7
gbrooks <griffinbrooks47@gmail.com>
2025-10-22 13:49:21 -0500

    auth url fix

--- auth-client.ts
+++ auth-client.ts
@@ -3,6 +3,6 @@ import type { auth } from "./lib/auth.ts";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     plugins: [inferAdditionalFields<typeof auth>()],
 });
